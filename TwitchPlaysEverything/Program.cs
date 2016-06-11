e231c738d546bf3d96c308e1d8d18ce021f4729e
Tim Beukert ( Pumpitx ) <phenom98@googlemail.com>
2016-06-11 22:49:52 +0200

    Daten verändert

--- TwitchPlaysEverything/Program.cs
+++ TwitchPlaysEverything/Program.cs
@@ -9,10 +9,10 @@ namespace TwitchPlaysEverything
 
         static void Main(string[] args)
         {
-            string username = "Pumpitx";
-            string pass = "oauth:072rdattgjxd46sh6piv50lie4y1mr";
-            string room = "pumpitx";
-            string application = "notepad";
+            string username = "mmustermann";
+            string pass = "oauth:12345";
+            string room = "mmustermann";
+            string application = "bsnes";
 
             ChatClient chat = new ChatClient("irc.chat.twitch.tv", 6667, username, pass);
             chat.joinRoom(room);
